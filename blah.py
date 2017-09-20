b8656775efc199d5fd70532958c7b34339034f97
Russell Ney <rney@wharton.upenn.edu>
2017-09-20 17:46:04 -0400

    added lines

--- blah.py
+++ blah.py
@@ -6,3 +6,31 @@ print()
 sess = tf.Session()
 print(sess.run([node1, node2]))
 print()
+node3 = tf.add(node1, node2)
+print("node3:", node3)
+print("sess.run(node3):", sess.run(node3))
+#
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+adder_node = a + b # + provide a shortcut for tf.add(a,b)
+#
+print(sess.run(adder_node, {a: 3, b: 4.5}))
+print(sess.run(adder_node, {a: [1, 3], b: [2, 4]}))
+add_and_triple = adder_node * 3.
+print(sess.run(add_and_triple, {a: 3, b: 4.5}))
+# add variables for training
+W = tf.Variable([.3], dtype=tf.float32)
+b = tf.Variable([-.3], dtype=tf.float32)
+x = tf.placeholder(tf.float32)
+linear_model = W * x + b
+# initialize variables
+init = tf.global_variables_initializer()
+sess.run(init)
+# evaluate many values for x (since it is a placeholder)
+print(sess.run(linear_model, {x: [1, 2, 3, 4]}))
+# create a loss function measures how far apart the current model is from the provided data
+y = tf.placeholder(tf.float32)
+squared_deltas = tf.square(linear_model - y)
+loss = tf.reduce_sum(squared_deltas)
+print(sess.run(loss, {x: [1, 2, 3, 4], y: [0, -1, -3]}))
+# now try to reduce loss by adjusting 
